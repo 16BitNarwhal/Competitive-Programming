f3a90c7fefcc150fa25a079ec7dcbf74e3f5c7e7
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2024-02-02 23:54:38 -0500

    leetcode linked list cycle

---
+++ leetcode/linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast:
+            if fast.next:
+                fast = fast.next.next
+            else:
+                fast = fast.next
+            
+            if slow == fast:
+                return True
+                
+            slow = slow.next
+
+        return False
